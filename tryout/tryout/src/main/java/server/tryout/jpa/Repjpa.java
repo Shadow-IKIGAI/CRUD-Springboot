92e4120c2541bac9b13d5f7800a75407241223ac
Shadow-IKIGAI <muralimn93@gmail.com>
2025-02-25 16:33:13 +0530

    Delete tryout/tryout directory

--- tryout/tryout/src/main/java/server/tryout/jpa/Repjpa.java
+++
@@ -1,9 +0,0 @@
-package server.tryout.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import server.tryout.model.Product;
-
-public interface Repjpa extends JpaRepository<Product,Integer>{
-
-}
